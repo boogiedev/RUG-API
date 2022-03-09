3800efbaf8aa68f5a45fe15c105eaead56e5d1a4
Wesley Nguyen <nguyenw@seattleu.edu>
2022-03-08 22:04:33 -0800

    went with pickle for ease

--- README.md
+++ README.md
@@ -13,7 +13,9 @@ Random User Generator(RUG) Statistics API
 
 ## What is this?
 
-Fill explanation
+Web service that has RESTful API endpoints for RUG data.
+
+Scope of the data was interpreted to only include UTF-8 names and all locations within the US.
 
 ---
 
@@ -21,12 +23,31 @@ Fill explanation
 
 ### Steps
 
-- [ ] 1: Determine technologies needed for application
+- [X] 1: Determine technologies/production stack needed for application
+  - Flask, Gunicorn (WSGI), AWS Elastic Beanstalk, Pandas (Data Processing)
+- [X] 2: Flesh out web service (figure out the endpoint(s))
+  - Final endpoint - /get_statistics
+  - Before requests, check if there is current (cleaned) data resource
+    - Call /fill_data if not
+  - Verify Stats & Data with /view_data and /view_statistics endpoint
+- [X] 3: Build tests for all resources (header tests as well)
+  - Build manual tests
+    - Test for ACCEPT headers (user shouldnt have to specify format, automatically determined)
+      - application/json
+      - text/xml
+      - text/plain
+      - 406 Error for non compatible header
+- [X] 4: Deploy (hopefully with CI/CD tools)
+  - Elastic Beanstalk connected to repo
 
 
 ### Features
 
-- [ ] Fill Intended Features
+- Routes/Endpoints "/api/v0" {params}
+  - [ ] fill_data {n_results} [GET] (Internal Endpoint, REFACTORED out of VIEW)
+  - [ ] view_data [GET]
+  - [ ] get_statistics {format} [GET]
+  - [ ] view_statistics [GET]
 
 ---
 
--- api/application.py
+++ api/application.py
@@ -1,3 +1,6 @@
+import os
+from os.path import exists
+import pandas as pd
 from process import calculate
 from flask import Flask, Response, jsonify, request
 import requests
@@ -8,9 +11,7 @@ application = Flask(__name__)
 VERSION = 0
 API_ROUTE = f'/api/v{VERSION}'
 
-
-# @application.route(API_ROUTE + '/fill_data', methods=['GET'])
-def fill_data(n_results:int=5000):
+def get_data(n_results:int=5000):
     """
     Route requests data from RUG API and stores processed data (persists until new call)
     """
@@ -19,21 +20,46 @@ def fill_data(n_results:int=5000):
     data = calculate.convert_data(response.json())
     return data
 
+# Before Request Wrapper
+@application.before_request
+def create_data():
+    path = 'data.pkl'
+    file_exists = exists(path)
+    global DATA
+    if not file_exists:
+        DATA = get_data()
+        DATA.to_pickle(path)
+    else:
+        DATA = pd.read_pickle(path)
+# Reset Data
+@application.route(API_ROUTE + '/reset_data')
+def reset_data():
+    """
+    Resets pickled data
+    """
+    path = 'data.pkl'
+    file_exists = exists(path)
+    if file_exists:
+        os.remove(path)
+    return 'data reset'
+
+# View for data as HTML
 @application.route(API_ROUTE + '/view_data')
 def view_data():
     """
-    Retrieves previous RUG API request and displays cleaned data
+    Retrieves previous RUG API request and displays cleaned data for verification
     """
     # Retrieve DF Data
-    data = fill_data()
-    return data.to_html()
+    return DATA.to_html()
 
+# Route to get stats in different formats
 @application.route(API_ROUTE + '/get_statistics')
 def get_statistics():
     """
     Returns statistics generated from data in user specified format (default=json)
     """
-    data = 'get from DB'
+    # Generate Stats from Saved DF
+    stats = calculate.generate_statistics(DATA)
     # Retrieve ACCEPT
     accept = request.headers.get('Accept')
     print(accept)
@@ -43,13 +69,6 @@ def get_statistics():
     "406" # Does not support any other type for accept header
     return data
 
-@application.route(API_ROUTE + '/view_statistics')
-def view_statistics():
-    """
-    Statistics Viewable online
-    """
-    # Retrieve DF Data
-    return 'view statistics'
 
 
 if __name__ == '__main__':
